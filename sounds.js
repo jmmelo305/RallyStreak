68f9b2e9fc89981567773aa28d5bfff43a679616
Melo <jmmelo.edu@gmail.com>
2026-03-12 08:55:27 -0400

    Delete sounds.js

--- sounds.js
+++
@@ -1,9 +0,0 @@
-// Tennis ball hit sound
-const tennisHitSound = new Audio();
-tennisHitSound.src = 'sounds/Tennis Ball Hit - Wii SPORTS TENIS - Sound Effect-[AudioTrimmer.com].mp3';
-tennisHitSound.volume = 0.6;
-
-function playHitSound() {
-  tennisHitSound.currentTime = 0;
-  tennisHitSound.play().catch(e => console.log('Audio play failed:', e));
-}
